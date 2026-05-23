6515e11d5e633b3798fc0eb5f92143b248fa47fa
Thomas Newman <tnewman057@gmail.com>
2026-05-23 02:01:04 +0000

    test

--- sketch/sketch.ino
+++ sketch/sketch.ino
@@ -1,9 +1,22 @@
+#include <Arduino_LED_Matrix.h>
+#include <Modulino.h>
+
+#include "numbers_frames.h"
+
+Arduino_LED_Matrix matrix;
+ModulinoKnob knob;
+
 void setup() {
   // put your setup code here, to run once:
+  matrix.begin();
+  matrix.clear();
 
+  Modulino.begin();
+  
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-
+  
+  matrix.renderBitmap(frame, 8, 13);
 }
